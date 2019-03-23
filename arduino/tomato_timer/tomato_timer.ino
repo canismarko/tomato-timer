cc8528c5a6693c21f617c2afece9b5f4ca12c1c1
Mark Wolfman <canismarko@gmail.com>
2019-03-22 21:29:54 -0500

    Re-wrote Arduino firmware for the new neopixel.

--- arduino/tomato_timer/tomato_timer.ino
+++ arduino/tomato_timer/tomato_timer.ino
@@ -29,11 +29,12 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_STRIP, STRIP_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_STRIP, STRIP_PIN, NEO_RGBW + NEO_KHZ800);
 Adafruit_NeoPixel ring = Adafruit_NeoPixel(N_RING, RING_PIN, NEO_GRB + NEO_KHZ800);
 
 // COLORS
 #define BRIGHTNESS 10 // 0-100
+#define BLUE strip.Color(0, 0, 255)
 #define PURPLE strip.Color(255, 0, 255)
 #define TEAL strip.Color(0, 128, 64)
 #define BLACK strip.Color(0, 0, 0)
@@ -42,68 +43,93 @@ Adafruit_NeoPixel ring = Adafruit_NeoPixel(N_RING, RING_PIN, NEO_GRB + NEO_KHZ80
 #define C_WORK PURPLE
 #define C_REST strip.Color(0, 255, 0)
 
-// Durations for each phase (in milliseconds)
-// Eg. 5 * 60 * 1000 --> 5 min
-#define TICK 400
-#define DEBUG_SPEEDUP 100 // Set to 1 for normal operation
-#define WORK_TIME 25 * 60 * 1000ul / DEBUG_SPEEDUP
-#define BREAK_TIME 5 * 60 * 1000ul / DEBUG_SPEEDUP
-#define BREAK_TIME_LONG BREAK_TIME * 4
+// Durations for each phase
+#define TICK 400 // In milliseconds
+#define DEBUG_SPEEDUP 1 // Set to 1 for normal operation
+const unsigned long WORK_SEC = 25 * 60; // 25 min (in seconds)
+const unsigned long BREAK_SEC = 5 * 60; // 5 min (in seconds)
+#define SHORT_PER_LONG 3 // How many short breaks to each long break
 
 #define TONE_HZ 200
 
 // Possible states for the system
-#define WORKING 0
-#define BREAK 1
-#define LONG_BREAK 2
-#define EXPIRED 3
-#define STOPPED 4
+#define STOPPED 0
+#define STARTING 1
+#define RUNNING 2
+#define BREAK 3
+#define EXPIRED 4
+#define STOPPING 5
 
 // Button and buzzer pin assignments
 #define BTN_START 2
 #define BTN_STOP 3
-// #define BTN_PIN 2
-// #define BTN_STOP 19
-// #define MUTE_PIN 2
 #define BUZZER A1
 
-// LED pin assignments
-#define RED0 7
-#define RED1 5
-#define RED2 4
-#define RED3 6
-#define RED4 8
-#define GRN0 10
-#define GRN1 11
-#define GRN2 12
-#define GRN3 13
-#define GRN4 9
-
-// Define pins for the red and green LED's
-const int N_REDS = 5;
-const int RED_PINS[N_REDS] = {RED0, RED1, RED2, RED3, RED4};
-const int N_GRNS = 5;
-const int GRN_PINS[N_GRNS] = {GRN0, GRN1, GRN2, GRN3, GRN4};
 
 // Variable for keeping track of how long this round has been running
 unsigned long startTime;
+char state;
+long roundNum;
 
-// Variable to keep track of which round of work we're on
-unsigned long currentRound;
-bool isStopped; // Are we currently paused
-bool volatile btnPressed;
-bool volatile useBuzzer;
-bool volatile stopRequested; // Is the system waiting for a stop button handler
 
-// Function to keep track of when the button was pressed
-void press_button() {
+// Function to start a work sesstion when the user presses the button
+void start_pressed() {
   Serial.print("Start button pressed\n");
-  btnPressed = true;
+  if (state != RUNNING) {
+    state = STARTING;
+  }
 }
+
 
 void stop_pressed() {
   Serial.print("Stop requested\n");
-  stopRequested = true;
+  state = STOPPING;
+}
+
+void startTimer() {
+  noTone(BUZZER);
+  state = RUNNING;
+  startTime = millis();
+  roundNum = roundNum + 1;
+  Serial.print("Starting round ");
+  Serial.print(roundNum);
+  Serial.print(" at ");
+  Serial.print(DEBUG_SPEEDUP);
+  Serial.println("x speed");
+}
+
+
+void stopTimer() {
+  roundNum = 0;
+  noTone(BUZZER);
+  flourish();
+  state = STOPPED;
+}
+
+
+void setAllPixels(Adafruit_NeoPixel &pixel, uint32_t c, bool keepBluePin=false) {
+  // Set all the neopixels to the given color
+  for (uint16_t i=0; i < pixel.numPixels(); i++) {
+    pixel.setPixelColor(i, c);
+  }
+  // Set the 12th pin to blue, for the ring timer
+  if (keepBluePin) {
+    pixel.setPixelColor(pixel.numPixels()-1, BLUE);
+  }
+  pixel.show();  
+}
+
+
+void setRingLights(int numOn, uint32_t color) {
+  // Set a certain number of LEDs on the ring to on
+  for (int i=0; i<numOn; i++) {
+    ring.setPixelColor(i, color);
+  }
+  // Set the remaining LEDs off
+  for (int i=numOn; i<N_RING; i++) {
+    ring.setPixelColor(i, RING_OFF);
+  }
+  ring.show();
 }
 
 
@@ -123,98 +149,122 @@ uint32_t Wheel(byte WheelPos) {
 }
 
 
-// Function for advancing to the next round of work
-void start_next_round() {
-  // Flash the lights to indicate the round number
-  if (currentRound > 0) {
-    sound_buzzer(LOW);
-    setAllPixels(ring, RING_OFF);
-    setAllPixels(strip, STRIP_OFF);
-    delay(TICK / 2);
-    for (int j=0; j<currentRound; j += 1) {
-      // sound_buzzer(HIGH);
-      setAllPixels(ring, C_WORK);
-      setAllPixels(strip, C_WORK);      
-      delay(TICK / 5);
-      sound_buzzer(LOW);
-      setAllPixels(ring, RING_OFF);
-      setAllPixels(strip, STRIP_OFF);
-      delay(TICK);
-    }
-    delay(TICK / 5);
-  }
-  // Log to serial console
-  Serial.print("Advancing to round ");
-  Serial.print(currentRound);
-  Serial.print("\n");
-  // Update state variables
-  isStopped = false;
-  currentRound += 1;
-  startTime = millis();
-  btnPressed = false;
-  useBuzzer = true;
-  // Turn off buzzer and pause for a moment
-  noTone(BUZZER);
-  delay(2*TICK);
+bool isLongRound(long roundNum) {
+  return (roundNum % (SHORT_PER_LONG + 1) == 0);
 }
 
 
-// Global variable for whether to sound the buzzer (gets reset every round)
-void mute_buzzer() {
-  Serial.print("Silencing buzzer\n");
-  useBuzzer = false;
-}
-
-// Function that either turns on or turns off the buzzer
-void sound_buzzer(int state) {
-  if (state == HIGH && useBuzzer) {
-    tone(BUZZER, TONE_HZ);
+// Determine if the timer is running or on break
+char getNewState(unsigned long mSeconds, long roundNum) {
+  // Get the new state
+  char newState;
+  if (state == STOPPED || state == STOPPING || state == STARTING) {
+    // We're stopped, so stay stopped.
+    newState = state;
+  } else if (mSeconds < WORK_SEC * 1000) {
+    // User is working normally
+    newState = RUNNING;
+  } else if (mSeconds < (WORK_SEC + BREAK_SEC) * 1000 && !isLongRound(roundNum)) {
+    // User is in a short break
+    newState = BREAK;
+  } else if (mSeconds < (WORK_SEC + (SHORT_PER_LONG+1) * BREAK_SEC) * 1000 && isLongRound(roundNum)) {
+    // User is in a long break
+    newState = BREAK;
   } else {
-    noTone(BUZZER);
+    // Break has ended
+    newState = EXPIRED;
   }
+  return newState;
 }
 
 
-// Function sets all pins in an arrayset
-void writeRedPins(int state) {
-  for (int i = 0; i < N_REDS; i += 1) {
-    digitalWrite(RED_PINS[i], state);
+// Set the number of lights on the timer depending on the number of seconds in we are.
+void setTimerLights(unsigned long mSeconds, long roundNum, float workBrightnessAdjust=1.) {
+  int pin;
+  int effectivePin;
+  bool isBreakPin;
+  double brightness;
+  uint32_t color;
+  unsigned long pinOffset = mSeconds / 1000 / 300;
+  // Disable the buzzer
+  noTone(BUZZER);
+  // Set the pins one at a time
+  for (pin=0; pin<11; pin++) {
+    // Determine the brightness for the pin
+    brightness = exp(-pin/4.655);
+    // Determine which color to make the pin
+    effectivePin = pin + pinOffset;
+    // Determine if we're in a break of not
+    if (isLongRound(roundNum)) {
+      // It's a long break round
+      isBreakPin = ((5 <= effectivePin && effectivePin <= 8) || effectivePin == 14);
+    } else if (roundNum % (SHORT_PER_LONG + 1) == 3) {
+      // It's the round right before a long break round
+      isBreakPin = (effectivePin == 5 || (11 <= effectivePin && effectivePin <= 14));
+    } else {
+      // It's a regular short break round
+      isBreakPin = (effectivePin == 5 || effectivePin == 11);
+    }
+    if (isBreakPin) {
+      // Break pixel
+      color = ring.Color(0, 255 * brightness, 0); // Green
+    } else {
+      // Work pixel
+      brightness = brightness * workBrightnessAdjust;
+      color = ring.Color(255 * brightness, 0, 255 * brightness);
+    }
+    // Set the actual pin color
+    ring.setPixelColor(pin, color);
   }
-}
 
-
-// Function sets all pins in an array
-void writeGreenPins(int state) {
-  for (int i = 0; i < N_GRNS; i += 1) {
-    digitalWrite(GRN_PINS[i], state);
-  }
-}
-
-
-void setAllPixels(Adafruit_NeoPixel &pixel, uint32_t c) {
-  // Set all the neopixels to the given color
-  for (uint16_t i=0; i < pixel.numPixels(); i++) {
-    pixel.setPixelColor(i, c);
-  }
-  pixel.show();  
-}
-
-
-void setRingLights(int numOn, uint32_t color) {
-  // Set a certain number of LEDs on the ring to on
-  for (int i=0; i<numOn; i++) {
-    ring.setPixelColor(i, color);
-  }
-  // Set the remaining LEDs off
-  for (int i=numOn; i<N_RING; i++) {
-    ring.setPixelColor(i, RING_OFF);
-  }
+  // Set the 12 o'clock position to be blue
+  ring.setPixelColor(pin, BLUE);
   ring.show();
 }
 
 
+void setWorkLights(unsigned long mSeconds, long roundNum) {
+  return setTimerLights(mSeconds, roundNum);
+}
+
+
+// Set the number of lights on the timer depending on how long the user is taking a break
+void setBreakLights(unsigned long mSeconds, long roundNum) {
+  unsigned long breakMillis;
+  breakMillis = mSeconds - WORK_SEC * 1000;
+  // Check if we should flash the lights
+  if (breakMillis / DEBUG_SPEEDUP < 6 * TICK) {
+    flashLights(breakMillis, C_REST);
+  } else {
+    // Display how much break time there is left
+    setTimerLights(mSeconds, roundNum, 0.);
+  }
+}
+
+
+void setExpiredLights(unsigned long mSeconds) {
+  unsigned long expiredMillis = mSeconds - (WORK_SEC + BREAK_SEC) * 1000;
+  flashLights(expiredMillis, C_WORK);
+}
+
+
+void flashLights(unsigned long mSeconds, uint32_t color) {
+  // Flash to lights to indicate the break has start
+  unsigned long numTicks = mSeconds / DEBUG_SPEEDUP / TICK;
+  if (numTicks % 2) {
+    // Odd tick
+    setAllPixels(ring, RING_OFF, true);
+    noTone(BUZZER);
+  } else {
+    // Even tick
+    setAllPixels(ring, color, true);
+    tone(BUZZER, TONE_HZ);
+  }
+}
+
+
 // Function that fluorishes the lights, then waits for the user to press the "Go" button
-void pauseInit() {
+void flourish() {
   Serial.print("Pausing init\n");
   // Cycle through the lights
   for (byte i=0; i < 255; i=i+1) {
@@ -262,118 +312,37 @@ void setup() {
   
   // Set the interrupt for when the various buttons are pressed
   // attachInterrupt(digitalPinToInterrupt(MUTE_PIN), mute_buzzer, RISING);
-  attachInterrupt(digitalPinToInterrupt(BTN_START), press_button, RISING);
+  attachInterrupt(digitalPinToInterrupt(BTN_START), start_pressed, RISING);
   attachInterrupt(digitalPinToInterrupt(BTN_STOP), stop_pressed, RISING);
 
-  // Start of on the first round
-  isStopped = true;
-  currentRound = 0;
-
-  // Wait for the user to press the go button
-  pauseInit();
-
-  // And away we gooooo....
-  startTime = millis();
+  // Set initial state
+  stopTimer();
 }
-
-// Function that returns the current state based on elapsed time for this round
-int currentState() {
-  unsigned long elapsed = millis() - startTime;
-  // Variable for holding the output
-  int state;
-  // Variable for how long the break should be this time around
-  unsigned long round_time;
-  // Variable for which break period we're in
-  int break_type;
-  if (currentRound % 4) {
-    // Regular short break
-    round_time = WORK_TIME + BREAK_TIME;
-    break_type = BREAK;
-  } else {
-    // Every fourth round is a long break
-    round_time = WORK_TIME + BREAK_TIME_LONG;
-    break_type = LONG_BREAK;
-  }
-  // Now see what phase we're in based on how long we've been running
-  if (isStopped) {
-    state = STOPPED;
-  } else if (elapsed < WORK_TIME) {
-    // Just regular old working mode
-    state = WORKING;
-  } else if (elapsed < round_time) {
-    // We're in a break
-    state = break_type;
-  } else {
-    // Break is over, and we need to get back to work!
-    state = EXPIRED;
-  }
-  return state;
-}
-
 
 void loop() {
 
-  int state = currentState();
-  
-  // Check if the user pressed a button during the last loop
-  if (btnPressed) {
-    if (state == WORKING && currentRound > 0) {
-      currentRound -= 1;
-    }
-    start_next_round();
-  }
+  // Figure out how long we've been working so far
+  unsigned long mSecondsIn = (millis() - startTime) * DEBUG_SPEEDUP;
 
-  unsigned long runTime = millis() - startTime;
-  char tickPhase = (runTime) % (TICK * 2) / TICK;
-  if (stopRequested) {
-    Serial.print("Stopping\n");
-    isStopped = true;
-    stopRequested = false;
-    sound_buzzer(LOW);
-    writeGreenPins(LOW);
-    writeRedPins(LOW);
-    if (state == WORKING && currentRound > 0) {
-      currentRound -= 1;
-    }
-    // Wait for the user to advance to the next round
-    pauseInit();
-    // And away we gooooo....
-    startTime = millis();
-  } else if (state == WORKING) {
-    // Set the LEDs while working
-    int n_leds = (WORK_TIME - runTime) / (WORK_TIME / (N_RING)) % (N_RING);
-    setRingLights(n_leds + 1, C_WORK);
-    setAllPixels(strip, C_WORK);
+  state = getNewState(mSecondsIn, roundNum);
+
+  // Set the lights appropriately
+  if (state == STOPPED) {
+    setAllPixels(strip, STRIP_OFF);
+    setAllPixels(ring, RING_OFF);
+  } else if (state == RUNNING) {
+    setWorkLights(mSecondsIn, roundNum);
   } else if (state == BREAK) {
-    // Set the LEDs while taking a short break
-    int n_leds = (WORK_TIME + BREAK_TIME - runTime) / (WORK_TIME / (N_RING)) % (N_RING);
-    setRingLights(n_leds + 1, C_REST);
-    setAllPixels(strip, C_REST);
-  } else if (state == LONG_BREAK) {
-    // Set the LEDs while taking a long break
-    int n_leds = (WORK_TIME + BREAK_TIME_LONG - runTime) / (WORK_TIME / (N_RING)) % (N_RING);
-    setRingLights(n_leds + 1, C_REST);
-    setAllPixels(strip, C_REST);
+    setBreakLights(mSecondsIn, roundNum);
   } else if (state == EXPIRED) {
-    sound_buzzer(tickPhase);
-    if (tickPhase == HIGH) {
-      setAllPixels(strip, C_WORK);
-      setAllPixels(ring, C_WORK);
-    } else {
-      setAllPixels(strip, STRIP_OFF);
-      setAllPixels(ring, RING_OFF);
-    }
-    // writeRedPins(tickPhase);
-    // writeGreenPins(LOW);
+    setExpiredLights(mSecondsIn);
+  } else if (state == STOPPING) {
+    stopTimer();
+  } else if (state == STARTING) {
+    startTimer();
   }
 
-  // Check if the buzzer should sound to alert that a break has just started
-  if (runTime > WORK_TIME && runTime < WORK_TIME + 6*TICK) {
-    sound_buzzer(tickPhase);
-  } else if (state != EXPIRED) {
-    sound_buzzer(LOW);
-  }
-
+  // Sleep until the next round
   delay(TICK);
 }
 
